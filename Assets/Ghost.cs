ba693dde18b2f5506d0bef9d1aaeb6abe8e53b79
Revolver1388 <45246957+Revolver1388@users.noreply.github.com>
2018-11-28 18:27:48 -0500

    Adding Unity Project//Kyle

---
+++ Assets/Ghost.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost : MonoBehaviour
+{
+
+    public Transform[] Waypoint;
+    public int currentLocation = 0;
+    public float speed = 3;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y != Waypoint[currentLocation].transform.position.y)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Waypoint[currentLocation].transform.position, speed * Time.deltaTime);
+        }
+        if (transform.position.y == Waypoint[currentLocation].transform.position.y)
+        {
+            currentLocation += 1;
+        }
+        if (currentLocation >= Waypoint.Length)
+        {
+            currentLocation = 0;
+        }
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public float speedX = 3;
+    public float speedY = 3.4f;
+
+    private Rigidbody2D body;
+
+	// Use this for initialization
+	void Start () {
+     body = GetComponent<Rigidbody2D>();
+
+	}
+
+    private void FixedUpdate()
+    {
+        float moveH = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveH, moveY);
+        body.velocity = speedX * movement;
+
+    }
+    // Update is called once per frame
+    void Update () {
+		
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 180);
+        }
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+    }
+}
